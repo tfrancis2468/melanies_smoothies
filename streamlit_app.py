e6d718d0a585fafbf85d31997297f23badf23bd1
tfrancis2468 <timothy.francis@daimlertruck.com>
2025-09-25 13:50:40 -0700

    Update streamlit_app.py
    Adding API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,3 +53,7 @@ if ingredients_list:
     #if ingredients_string:
         #session.sql(my_insert_stmt).collect()
         #st.success('Your Smoothie is ordered!', icon="✅")
+# New section to display fruityvice nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
